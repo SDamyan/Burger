9aeb8a815c237cf0cec0b930aaff94fd405cae0f
Sara Elin <sedamyan@gmail.com>
2020-01-30 17:06:52 -0600

    installed json package, express, mysql, and created db schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS burger_DB;
+CREATE DATABASE burger_DB;
+USE burger_DB;
+
+CREATE TABLE burgers (
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (50) NOT NULL,
+    devored BOOLEAN NOT NULL,
+     PRIMARY KEY (id)
+);
