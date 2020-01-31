526e635fe8838b803a0aea4119e8da30187d4188
Sara Elin <sedamyan@gmail.com>
2020-01-31 13:43:34 -0600

    set up db and config folders and files

--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,9 @@
 INSERT INTO burgers (burger_name, devoured) 
     VALUES ("Bacon, egg, and cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ("Veggie burger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+    VALUES ("Turkey burger", false);
+
